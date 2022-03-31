22730bbf315b917fc1acec240bf5c91b9cac4efa
xSuikun <suykun228@gmail.com>
2022-04-01 01:49:47 +0300

    some fixes

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -131,7 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -178,4 +184,3 @@ LOGGING = {
     }
 }
 
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
