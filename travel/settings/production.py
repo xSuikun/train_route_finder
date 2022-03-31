b2ad4b5ffe3df256c363b2e98f9e251d4fcc82d6
xSuikun <suykun228@gmail.com>
2022-04-01 01:54:44 +0300

    some fixes

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -131,12 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
