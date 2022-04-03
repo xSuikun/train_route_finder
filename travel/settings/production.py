9bf51c0f48be828e52e2217c45c27fbfc1b97398
xSuikun <suykun228@gmail.com>
2022-04-03 04:39:53 +0300

    added error pages

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -132,6 +132,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
